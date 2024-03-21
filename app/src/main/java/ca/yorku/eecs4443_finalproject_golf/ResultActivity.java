fa28be11a5931eb71d56457a31df7e22c24f8450
Omar Ali <61897650+omar0ali@users.noreply.github.com>
2024-03-21 16:44:36 -0400

    Added a button to clear saved CSV data in the ResultActivity. Also, implemented a button in the SetupActivity to smoothly navigate to the ResultsActivity.

--- app/src/main/java/ca/yorku/eecs4443_finalproject_golf/ResultActivity.java
+++ app/src/main/java/ca/yorku/eecs4443_finalproject_golf/ResultActivity.java
@@ -1,18 +1,25 @@
 package ca.yorku.eecs4443_finalproject_golf;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import java.util.Arrays;
 import androidmads.library.qrgenearator.QRGContents;
 import androidmads.library.qrgenearator.QRGEncoder;
 
 public class ResultActivity extends AppCompatActivity {
 
+    Button btnClear;
     TextView txtTitle;
     ListView lsResults;
     ImageView imgQRCode;
@@ -25,20 +32,43 @@ public class ResultActivity extends AppCompatActivity {
         txtTitle = (TextView) findViewById(R.id.lbl_results_title);
         lsResults = (ListView) findViewById(R.id.list_results);
         imgQRCode = (ImageView) findViewById(R.id.qr_code);
+        btnClear = (Button) findViewById(R.id.btn_clear);
+
+        displayResults(this, lsResults);
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ResultActivity.this);
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        SharedPref.clearCSVData(ResultActivity.this);
+                        displayResults(ResultActivity.this, lsResults);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {} //Ignore
+                });
+                AlertDialog dialog = builder.create();
+                dialog.setTitle("Clear CSV data");
+                dialog.setMessage("Are you sure?");
+                dialog.show();
+            }
+        });
+    }
 
 
+    public void displayResults(Context context, ListView lsResults) {
         String results = SharedPref.getCSVData(this);
-        String[] list_result = results.split("\n");
-        for (int i = 0;i<list_result.length;i++){
-            list_result[i] = list_result[i].replace(",", "\t\t");
-        }
+        String[] list_result = Arrays.stream(results.split("\n"))
+                .map(line -> line.replace(",", "\t\t"))
+                .toArray(String[]::new);
         lsResults.setAdapter(
                 new ArrayAdapter<String>(
                         this,
                         R.layout.listview_item,
                         list_result
                 ));
-
 
         /**
          * Library Used: https://github.com/androidmads/QRGenerator
@@ -55,6 +85,5 @@ public class ResultActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
--- app/src/main/java/ca/yorku/eecs4443_finalproject_golf/SetupActivity.java
+++ app/src/main/java/ca/yorku/eecs4443_finalproject_golf/SetupActivity.java
@@ -3,6 +3,7 @@ package ca.yorku.eecs4443_finalproject_golf;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +17,10 @@ public class SetupActivity extends AppCompatActivity {
 
         // Event listener for "Get Started" button
         findViewById(R.id.getStartedButton).setOnClickListener(this::getStarted);
+
+        // Show results activity
+        Button btnShowResults = (Button) findViewById(R.id.btn_show_results);
+        btnShowResults.setOnClickListener(v -> startActivity(new Intent(SetupActivity.this, ResultActivity.class)));
     }
 
     private void getStarted(View v) {
